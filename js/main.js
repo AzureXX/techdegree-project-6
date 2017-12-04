afa4022c543e294d41a20ab10a32db2de2202dbd
AzureXX <ismat.valiyev@gmail.com>
2017-12-04 14:33:50 +0400

    Almost done

---
+++ js/main.js
@@ -0,0 +1,31 @@
+$('video').mediaelementplayer({
+  features: ["playpause", "current", "progress", "duration", "volume", "fullscreen"],
+});
+
+
+$("span").click( (e) => {
+  $('video').mediaelementplayer.currentTime = e.target.startTime;
+});
+
+
+
+
+const video = $('video')[0];
+const spans = $('p span');
+
+video.addEventListener('timeupdate', () => {
+  for (let i = 0; i < spans.length; i += 1) {
+    let startTime = spans[i].getAttribute('startTime');
+    let endTime = spans[i].getAttribute('endTime');
+    let currentTime = video.currentTime;
+    if (currentTime >= startTime && currentTime <= endTime) {
+      spans[i].classList.add('highlight');
+    } else {
+      spans[i].classList.remove('highlight');
+    }
+  }
+});
+
+spans.click( (e) => {
+    video.currentTime = e.target.getAtribute('startTime');
+});
